716d9c1287573decb21f9711dfc606aa6e0fed30
dmitry <vbnz1>
2017-05-15 22:37:30 +0200

    #7: H2 in memory database setup for test

--- src/test/groovy/gq/erokhin/databee/DataBeeTest.groovy
+++ src/test/groovy/gq/erokhin/databee/DataBeeTest.groovy
@@ -1,32 +1,39 @@
 package gq.erokhin.databee
 
+import groovy.sql.Sql
 import spock.lang.Specification
-
-import static java.lang.System.out
 
 /**
  *  Created by Dmitry Erokhin (dmitry.erokhin@gmail.com)
  *  15.05.17
  */
 class DataBeeTest extends Specification {
+    def static DB_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;MODE=PostgreSQL"
+    def static SAMPLE_ROW_COUNT = 1000;
+    private Sql sql
 
     void setup() {
-        out.println "Set up"
+        sql = Sql.newInstance(DB_URL)
+        sql.execute('CREATE TABLE test (id SERIAL PRIMARY KEY, data TEXT)')
+        sql.withBatch { stmt ->
+            SAMPLE_ROW_COUNT.times {
+                stmt.addBatch("INSERT INTO test(data) VALUES('test data #$it')")
+            }
+        }
     }
 
     void cleanup() {
-        out.println "cleanUp"
     }
 
     def "Sample test"() {
         setup:
-        def x = 1
+        def data
 
         when:
-        x++
+        data = sql.rows("SELECT count(*) AS count FROM test")
 
         then:
-        x == 2
+        data[0].count == SAMPLE_ROW_COUNT
     }
 
 }
